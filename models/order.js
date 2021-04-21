65a212fc67a9d3866d6b1aefb0fe4829963eddad
klinarto <kevinlinarto@gmail.com>
2021-04-21 21:40:28 +0800

    Implemented more features for orderController
    - changed orderSchema

--- models/order.js
+++ models/order.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 
-const OrderSchema = new mongoose.Schema({
-	orderId: { type: String, required: true },
-	customerID: { type: mongoose.ObjectId, required: true },
-	foodItems: { type: Object, required: true },
-	status: { type: String, required: true },
-});
+const OrderSchema = new mongoose.Schema(
+	{
+		orderId: { type: String, required: true },
+		customerID: { type: mongoose.ObjectId, required: true },
+		foodItems: { type: Object, required: true },
+		status: { type: String, required: true },
+		orderCost: { type: Number, required: true },
+		totalCost: { type: Number, required: true },
+	},
+	{ timestamps: true }
+);
 
 module.exports = Order = mongoose.model("Order", OrderSchema, "orders");
