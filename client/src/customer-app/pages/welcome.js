03d001e2e556c7860a59f85330ff4b956c7fcc69
John Kim <kakwak123@gmail.com>
2021-05-01 20:51:21 +1000

    modified welcome page

--- client/src/customer-app/pages/welcome.js
+++ client/src/customer-app/pages/welcome.js
@@ -1,16 +1,11 @@
-import { urlencoded } from "express";
 import React from "react";
 import { Container, LeftWrapper, RightWrapper } from "./welcome.style";
-
-var imageSrc = url("clientpubliclogo512.png");
 
 export function Welcome() {
   return (
     <Container>
       <LeftWrapper>asdfasdf</LeftWrapper>
-      <RightWrapper>
-        <img src={imageSrc} />
-      </RightWrapper>
+      <RightWrapper>asfdasdf</RightWrapper>
     </Container>
   );
 }
