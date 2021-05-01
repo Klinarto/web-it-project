5f795433f463c0001a666ec26eaa57089b9f6420
klinarto <kevinlinarto@gmail.com>
2021-05-01 17:48:42 +0800

    Remove .env file and fixed app listen port log

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.use("/vendor", require("./routes/vendor"));
 app.use("/order", require("./routes/order"));
 
 app.listen(port, () => {
-	console.log("The app is listening on port 3000!");
+	console.log(`The app is listening on port ${port}`);
 });
