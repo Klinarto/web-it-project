40b3731f6648f79085ef1825c36884f9b767e27d
John Kim <kakwak123@gmail.com>
2021-05-01 20:21:13 +1000

    asdf

--- client/src/shared/components/Header/Header.js
+++ client/src/shared/components/Header/Header.js
@@ -1,31 +1,41 @@
 import React from "react";
-import { HeaderWrapper, LogoText, ItemList, MyNavLink } from "./Header.styles";
+import { StylesProvider } from "@material-ui/core/styles";
+import {
+  HeaderWrapper,
+  LogoText,
+  ItemList,
+  MyNavLink,
+  MyButton,
+} from "./Header.styles";
 
 export function Header() {
   return (
-    <HeaderWrapper>
-      <LogoText>Snacks in a Van</LogoText>
-      <ItemList>
-        <li>
-          <MyNavLink to="/customer/vans">Vans</MyNavLink>
-        </li>
-        <li>
-          <MyNavLink to="/customer/orders">Orders</MyNavLink>
-        </li>
-        <li>
-          <MyNavLink to="/help">Help</MyNavLink>
-        </li>
-        <li>
-          <MyNavLink to="/contactus">Contact Us</MyNavLink>
-        </li>
-        <li>
-          <MyNavLink to="/customer/myaccount">My Accout</MyNavLink>
-        </li>
-        <li>
-          <MyNavLink to="/customer/signout">Sign out</MyNavLink>
-        </li>
-      </ItemList>
-    </HeaderWrapper>
+    <StylesProvider injectFirst>
+      <HeaderWrapper>
+        <MyButton>asdfasdfasdfasdf</MyButton>
+        <LogoText>Snacks in a Van</LogoText>
+        <ItemList>
+          <li>
+            <MyNavLink to="/customer/vans">Vans</MyNavLink>
+          </li>
+          <li>
+            <MyNavLink to="/customer/orders">Orders</MyNavLink>
+          </li>
+          <li>
+            <MyNavLink to="/help">Help</MyNavLink>
+          </li>
+          <li>
+            <MyNavLink to="/contactus">Contact Us</MyNavLink>
+          </li>
+          <li>
+            <MyNavLink to="/customer/myaccount">My Accout</MyNavLink>
+          </li>
+          <li>
+            <MyNavLink to="/customer/signout">Sign out</MyNavLink>
+          </li>
+        </ItemList>
+      </HeaderWrapper>
+    </StylesProvider>
   );
 }
 
--- client/src/shared/components/Header/Header.styles.js
+++ client/src/shared/components/Header/Header.styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 export const HeaderWrapper = styled.div`
   background-color: black;
@@ -37,5 +38,14 @@ export const MyNavLink = styled(NavLink)`
 
   &:hover {
     color: grey;
+  }
+`;
+
+export const MyButton = styled(Button)`
+  background-color: #aaa;
+  color: #000;
+  padding: 7px 14px;
+  &:hover {
+    background-color: #5469d4;
   }
 `;
