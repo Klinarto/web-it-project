e2c7823a9ccbe1b7bb60257047fda49102d161fb
Sonia Tam <soniat@student.unimelb.edu.au>
2021-05-04 21:56:49 +1000

    Update Menu with different Phone and PC work in progress

--- client/src/customer-app/pages/Menu.js
+++ client/src/customer-app/pages/Menu.js
@@ -1,101 +1,99 @@
-import { render } from "@testing-library/react";
-import React,{Component} from "react";
-//import React from "react";
-import styled from 'styled-components'
-
-export const Image = styled.img`
-  right: 0;
-  width: 100px;
-  height: 100px;
-  object-fit: cover;
-`;
+//import { render } from "@testing-library/react";
+import React, {useEffect, useState} from "react";
+import{
+  Image,DIV,TDPC,
+  Wrapper,TD,InTR,Price,ImageBig
+} from "./Menu.style";
+import styled from 'styled-components';
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 
-export const DIV = styled.div`
-  padding:10px;
-  /* width: 80%; */
-  align-self: center;
-  align-items:center;
-  justify-content: center;
-`;
+export default function Menu(){
+  const menu =getMenu()
+  const mobileSize = useMediaQuery(`(min-width: ${"768px"})`)
+  return (
+    <Wrapper>
+      <h1>Menu </h1>
+      <table>
+          {mobileSize ? renderLaptopMenu(menu) : renderPhoneMenu(menu)}
+      </table>
+    </Wrapper>
+  )
+}
 
-export const Table = styled.table`
-  align-self:center;
-  margin-left: auto;
-  margin-right: auto;
-`;
+function getMenu(){
+  return [
+    {"name": "cappuccino",
+    "price": 4.5,
+    "image": "https://images.unsplash.com/photo-1557006021-b85faa2bc5e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3034&q=80",
+    "detail": "a double shot of espresso with equal steamed milk and foam"
+    },
+  
+    {"name": "latte",
+    "price": 4.5,
+    "image": "https://images.unsplash.com/photo-1570968915860-54d5c301fa9f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3024&q=80",
+    "detail": "a double shot of espresso with steamed milk and small layer of foam"
+    },
+  
+    {"name": "flat white",
+    "price": 4.5,
+    "image": "https://images.unsplash.com/photo-1577968897966-3d4325b36b61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80",
+    "detail": "a double shot of espresso with flat steamed milk"
+    },
 
-export const TD = styled.td`
-  padding-top :5px;
-  padding-left :5px;
-  padding-right :5px;
-  padding-bottom:5px;
-
-`;
-
-export const Price = styled.span`
-  color: grey;
-`;
-
-
-class Menu extends Component{
-  constructor(props){
-    super(props)
-    this.state ={
-      dummy_dat:[
-        {"name": "cappuccino",
-        "price": 4.5,
-        "image": "https://images.unsplash.com/photo-1557006021-b85faa2bc5e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3034&q=80",
-        "detail": "a double shot of espresso with equal steamed milk and foam"
-        },
-
-        {"name": "latte",
-        "price": 4.5,
-        "image": "https://images.unsplash.com/photo-1570968915860-54d5c301fa9f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3024&q=80",
-        "detail": "a double shot of espresso with steamed milk and small layer of foam"
-        },
-
-        {"name": "flat white",
-        "price": 4.5,
-        "image": "https://images.unsplash.com/photo-1577968897966-3d4325b36b61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80",
-        "detail": "a double shot of espresso with flat steamed milk"
-        }
-      ]
+    {"name": "long black",
+    "price": 4.0,
+    "image": "https://images.unsplash.com/photo-1551030173-122aabc4489c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
+    "detail": "double expresso on hot water"
     }
-  }
-
-
-
-  renderTable(){
-    return this.state.dummy_dat.map((item,index) => {
-      const{ name, price , image, detail} = item
-      return(
-        <tr key = {name}>
-          <TD><p><b>{name} </b>
-          <Price>{price}</Price>
-          <br></br>
-          {detail}</p></TD>
-          <td><Image src ={image} /></td>
-        </tr>
-      )
-    }
-    )
-  }
-
-
-  render(){
-    return (
-      <DIV>
-        <h1>Menu </h1>
-        <Table>
-          <tbody>
-            {this.renderTable()}
-          </tbody>
-        </Table>
-      </DIV>
-    )
-  }
+    
+  ]
 }
 
 
-export default Menu;
+
+function renderLaptopMenu(menu){
+  const row = menu.map(item => renderItem(item))
+  return row
+   
+}
+
+function renderItem(item){
+  const{ name, price , image, detail} = item
+  return(
+    <TDPC>
+      <InTR><ImageBig src ={image} /></InTR>
+      <InTR>
+        <p>
+        <b>{name} </b>
+        <Price>{price}</Price>
+        <br></br>
+        {detail}
+        <br></br>
+        <hr></hr>
+        </p></InTR>
+    </TDPC>
+  )
+}
+
+
+
+function renderPhoneMenu(menu){
+  return menu.map(item => {const{name, price , image, detail} = item
+    return(
+      <table>
+        <tr><td><Image src ={image} /></td></tr>
+        <tr><TD><p>
+            <b>{name} </b>
+            <Price>{price}</Price>
+            <br></br>
+            {detail}
+            <br></br>
+            <hr></hr>
+        </p></TD></tr>
+      </table>
+    )
+    }
+  )
+}
+
---
+++ client/src/customer-app/pages/Menu.style.js
@@ -0,0 +1,67 @@
+
+
+import styled from 'styled-components';
+
+
+
+export const Image = styled.img`
+right: 0;
+width: 100px;
+height: 100px;
+object-fit: cover;
+`;
+
+export const ImageBig = styled.img`
+display: block;
+width: 300px;
+height: 200px;
+object-fit: cover;
+align-self:center;
+`;
+
+
+
+export const Wrapper = styled.div`
+padding:5%;
+/* width: 80%; */
+align-self: center;
+align-items:center;
+justify-content: center;
+`;
+
+
+export const DIV = styled.div`
+padding:10px;
+/* width: 80%; */
+align-self: center;
+align-items:center;
+justify-content: center;
+`;
+
+export const Table = styled.table`
+align-self:center;
+margin-left: auto;
+margin-right: auto;
+`;
+
+export const TD = styled.td`
+padding : 5px;
+height:100%;
+width: 100%;
+`;
+
+export const TDPC = styled.td`
+padding : 5px;
+height:25%vh;
+width: 25%vw;
+`;
+
+export const InTR = styled.tr`
+padding : 5px;
+height:100%;
+width: 100%;
+`;
+
+export const Price = styled.span`
+color: grey;
+`;
