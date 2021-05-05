a02ce0f5d08d48f238ef4cc7904d5cad03b2111a
Jun Hee Han <jun.han@student.unimelb.edu.au>
2021-05-05 22:27:38 +1000

    An eeor to be debugged on menu.js, page directory.

--- client/src/customer-app/pages/Menu.js
+++ client/src/customer-app/pages/Menu.js
@@ -41,75 +41,41 @@ export default function Menu() {
 		return () => {};
 	}, []);
 
-	return <h1>Test</h1>;
-	// const menu = getMenu();
-	// const mobileSize = useMediaQuery(`(min-width: ${"768px"})`);
-	// return (
-	// 	<Wrapper>
-	// 		<h1>Menu </h1>
-	// 		<table>
-	// 			{mobileSize ? renderLaptopMenu(menu) : renderPhoneMenu(menu)}
-	// 		</table>
-	// 	</Wrapper>
-	// );
+
+	const something = menu;
+	const mobileSize = useMediaQuery(`(min-width: ${"768px"})`);
+	return (
+		<Wrapper>
+			<h1>Menu</h1>
+			<hr/>
+			<br/>
+			<table>
+				{renderLaptopMenu(something['beverage'])}
+			</table>
+			<table>
+				{renderLaptopMenu(something['food'])}
+			</table>
+		</Wrapper>
+	);
 }
 
-function getMenu() {
-	return [
-		{
-			name: "cappuccino",
-			price: 4.5,
-			image:
-				"https://images.unsplash.com/photo-1557006021-b85faa2bc5e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3034&q=80",
-			detail:
-				"a double shot of espresso with equal steamed milk and foam",
-		},
-
-		{
-			name: "latte",
-			price: 4.5,
-			image:
-				"https://images.unsplash.com/photo-1570968915860-54d5c301fa9f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3024&q=80",
-			detail:
-				"a double shot of espresso with steamed milk and small layer of foam",
-		},
-
-		{
-			name: "flat white",
-			price: 4.5,
-			image:
-				"https://images.unsplash.com/photo-1577968897966-3d4325b36b61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80",
-			detail: "a double shot of espresso with flat steamed milk",
-		},
-
-		{
-			name: "long black",
-			price: 4.0,
-			image:
-				"https://images.unsplash.com/photo-1551030173-122aabc4489c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
-			detail: "double expresso on hot water at 90 degrees celcius ",
-		},
-	];
-}
-
-function renderLaptopMenu(menu) {
-	const row = menu.map((item) => renderItem(item));
+function renderLaptopMenu(array) {
+	const row = array.map((item) => renderItem(item));
 	return row;
 }
 
 function renderItem(item) {
-	const { name, price, image, detail } = item;
 	return (
 		<TDPC>
 			<InTR>
-				<ImageBig src={image} />
+				<ImageBig src={item['image']} />
 			</InTR>
 			<tr>
 				<p>
-					<b>{name} </b>
-					<Price>{price}</Price>
+					<b>{item['name']} </b>
+					<Price>{item['price']}</Price>
 					<br></br>
-					{detail}
+					{item['detail']}
 					<br></br>
 					<hr></hr>
 				</p>
@@ -118,25 +84,25 @@ function renderItem(item) {
 	);
 }
 
-function renderPhoneMenu(menu) {
-	return menu.map((item) => {
-		const { name, price, image, detail } = item;
-		return (
-			<tr>
-				<TD>
-					<p>
-						<b>{name} </b>
-						<Price>{price}</Price>
-						<br></br>
-						{detail}
-						<br></br>
-						{/* <hr></hr> */}
-					</p>
-				</TD>
-				<td>
-					<Image src={image} />
-				</td>
-			</tr>
-		);
-	});
-}
+// function renderPhoneMenu(menu) {
+// 	return menu.map((item) => {
+// 		const { name, price, image, detail } = item;
+// 		return (
+// 			<tr>
+// 				<TD>
+// 					<p>
+// 						<b>{name} </b>
+// 						<Price>{price}</Price>
+// 						<br></br>
+// 						{detail}
+// 						<br></br>
+// 						{/* <hr></hr> */}
+// 					</p>
+// 				</TD>
+// 				<td>
+// 					<Image src={image} />
+// 				</td>
+// 			</tr>
+// 		);
+// 	});
+// }
