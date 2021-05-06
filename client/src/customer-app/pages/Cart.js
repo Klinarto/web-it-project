2ed37126d3a6fbe41deb9f4fd1201a60291e58a9
Jun Hee Han <jun.han@student.unimelb.edu.au>
2021-05-07 00:15:52 +1000

    making order in implementation stage...

---
+++ client/src/customer-app/pages/Cart.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+
+
+export default function Cart() {
+    const orderList = JSON.parse(localStorage.getItem('order'));
+    console.log(orderList);
+
+    const makeOrder = async (order) => {
+		try {
+			const res = await axios.post("/order", order, {header:{"Content-Type":"application/json", 
+                                                                "x-access-token":localStorage.getItem('token')}});
+		}
+		catch(error) {
+			console.log(error.response.data);
+		}
+		return;
+	}
+
+    return (
+        <p>
+            <ul></ul>
+            <button onClick={() => {makeOrder(orderList)}}>make order</button>
+        </p>
+    );
+}
