6440f726c9c4e4ee23f156307e5627ff7eb6e461
soosoo3 <sooyoun1999@gmail.com>
2021-05-06 20:09:02 +1000

    rate page laptop version

--- client/src/customer-app/pages/Rate.style.js
+++ client/src/customer-app/pages/Rate.style.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-// import { device } from "../../shared/components/device";
+import { device } from "../../shared/components/device";
 
 export const Container = styled.div`
 
@@ -7,20 +7,55 @@ export const Container = styled.div`
 
 export const TopWrapper = styled.div`
 
-`;
-export const TopImage = styled.img`
+    margin-left: 38%;
+    margin-top: 5%;
+    @media ${device.tablet} {
+    flex: 0 0 60%;
+    max-width: 60%;
+  }
+
+  @media ${device.laptop} {
+    flex: 0 0 40%;
+    max-width: 40%;
+  }
+
+
 
 `;
+export const TopImage = styled.img`
+    width: 100px;
+    height: auto;
+    padding-left: 103px;
+
+    
+`;
+
 
 export const Title = styled.h1`
 
 `;
 
-export const Star = styled.h1`
 
-`;
 
 export const Button = styled.button`
+    margin-top: 10px;
+    margin-left: 102px;
+
+
+    display: block;
+  
+
+  background-color: black;
+  color: white;
+  padding: 7px 7px;
+  transition: background 0.5s ease-in-out;
+  width: 18%;
+  border-radius: 5px;
+  &:hover {
+    color: black;
+    background: #fff;
+    cursor: pointer;
+  }
 
 `;
 
