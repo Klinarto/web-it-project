2e3b6f47585fe9ad17e5e12d6e373be0df3e95ed
John Kim <kakwak123@gmail.com>
2021-05-08 00:14:37 +1000

    Changed cart directory

--- client/src/customer-app/pages/Order.js
+++ client/src/customer-app/pages/Order.js
@@ -2,107 +2,96 @@ import React, { useEffect, useState } from "react";
 import Interval from "../components/Interval";
 import coffeeMachine from "../../coffeeMachine.png";
 import {
-	Container,
-	Status,
-	Division,
-	OrderList,
-	OrderItem,
-	OrderPrice,
-	BreakLine,
-	DivisionBottom,
-	DiscountMessage,
-	Total,
-	TotalPrice,
-	Logo,
-	MyButton,
+  Container,
+  Status,
+  Division,
+  OrderList,
+  OrderItem,
+  BreakLine,
+  DivisionBottom,
+  DiscountMessage,
+  Total,
+  TotalPrice,
+  Logo,
+  MyButton,
 } from "./Order.style";
 import axios from "axios";
 import { useLocation } from "react-router";
 
 export function Order(props) {
-	const [menu, setMenu] = useState({});
+  const [menu, setMenu] = useState({});
 
-	// Quick solution to get a price: Fetch the whole menu data. Will be fixed soon.
-	useEffect(() => {
-		let isMounted = true;
-		const fetchMenu = async () => {
-			try {
-				const res = await axios.get("/menu");
-				if (isMounted) {
-					setMenu(res.data);
-				}
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		fetchMenu();
-		return () => {
-			isMounted = false;
-		};
-	}, [menu]);
+  // Quick solution to get a price: Fetch the whole menu data. Will be fixed soon.
+  useEffect(() => {
+    let isMounted = true;
+    const fetchMenu = async () => {
+      try {
+        const res = await axios.get("/menu");
+        if (isMounted) {
+          setMenu(res.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchMenu();
+    return () => {
+      isMounted = false;
+    };
+  }, [menu]);
 
-	const order = useLocation().state;
-	console.log(order);
-	const {
-		createdAt,
-		customerId,
-		foodItems,
-		orderCost,
-		orderId,
-		status,
-		totalCost,
-		updatedAt,
-		vendorId,
-	} = order;
+  const order = useLocation().state;
+  console.log(order);
+  const { foodItems, totalCost } = order;
 
-	var prices = [];
-	// console.log(foodItems);
-	// console.log(menu);
-	// console.log("menu type", typeof menu);
+  var prices = [];
+  // console.log(foodItems);
+  // console.log(menu);
+  // console.log("menu type", typeof menu);
 
-	return (
-		<Container>
-			<Status>Preparing your order...</Status>
-			<Division>
-				<div>
-					{Object.entries(foodItems).map(function ([item, quantity]) {
-						return (
-							<OrderList>
-								<OrderItem>
-									{quantity} x {item}
-								</OrderItem>
-							</OrderList>
-						);
-					})}
-				</div>
-				<div>
-					{prices.map(function (price) {
-						return (
-							<OrderList>
-								<OrderItem>$ {price}</OrderItem>
-							</OrderList>
-						);
-					})}
-				</div>
-			</Division>
-			<BreakLine />
-			<DivisionBottom>
-				<div>
-					<DiscountMessage>
-						20% discount applies if the order<br></br>takes more than 15 mins
-					</DiscountMessage>
-				</div>
-				<div>
-					<Total>Total</Total>
-					<TotalPrice>${totalCost}</TotalPrice>
-				</div>
-			</DivisionBottom>
-			<Logo alt="machine-logo" src={coffeeMachine} />
-			<Interval />
-			<MyButton>Change order</MyButton>
-			<MyButton>Cancel order</MyButton>
-		</Container>
-	);
+  return (
+    <Container>
+      <Status>Preparing your order...</Status>
+      <Division>
+        <div>
+          {Object.entries(foodItems).map(function ([item, quantity]) {
+            return (
+              <OrderList>
+                <OrderItem>
+                  {quantity} x {item}
+                </OrderItem>
+              </OrderList>
+            );
+          })}
+        </div>
+        <div>
+          {prices.map(function (price) {
+            return (
+              <OrderList>
+                <OrderItem>$ {price}</OrderItem>
+              </OrderList>
+            );
+          })}
+        </div>
+      </Division>
+      <BreakLine />
+      <DivisionBottom>
+        <div>
+          <DiscountMessage>
+            20% discount applies if the order<br></br>takes more than 15 mins
+          </DiscountMessage>
+        </div>
+        <div>
+          <Total>Total</Total>
+          <TotalPrice>${totalCost}</TotalPrice>
+        </div>
+      </DivisionBottom>
+      <Logo alt="machine-logo" src={coffeeMachine} />
+      <Interval />
+      <MyButton>Change order</MyButton>
+      <MyButton>Cancel order</MyButton>
+    </Container>
+  );
 }
 
 export default Order;
