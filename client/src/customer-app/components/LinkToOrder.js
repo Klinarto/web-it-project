9c84d5660fc0c9bbe278e58f0283d26cd624b91f
Jun Hee Han <jun.han@student.unimelb.edu.au>
2021-05-07 22:35:37 +1000

    LinkToOrder component separated from OrderHistory.

--- client/src/customer-app/components/LinkToOrder.js
+++ client/src/customer-app/components/LinkToOrder.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, OrderTitle,OrderList,FoodItem, Division, OrderItem, Title, LeftWrapper, MyButton } from "../pages/OrderHistory.style";
 import { Link } from "react-router-dom";
+import { parseDate } from "../utilities/Utils";
+
 
 export default function LinkToOrder(props) {
 	const {
@@ -15,38 +17,54 @@ export default function LinkToOrder(props) {
 		updatedAt,
 	} = props.order;
 
-  return (
-    <Container>
-      <Link to="/customer/order" style={{textDecoration: 'none', color: "black"}}>
-        <div>
-          <OrderTitle>Order {orderId} </OrderTitle> 
-          <hr/>
-          <Division>
-            <innerDiv>
-              <OrderList>
-                <OrderItem><b>Vendor:</b> {vendorId.name}</OrderItem>
-                <OrderItem><b>Status:</b> {status}</OrderItem>
-              </OrderList>
-            </innerDiv>
 
-            <innerDiv>
-              <OrderItem><b>Items ordered: </b></OrderItem>
-              {Object.keys(foodItems).map((key, i) => (
-                <FoodItem>
-                {foodItems[key]}  {key}
-                </FoodItem>
-              ))}
-            </innerDiv>
+	const parsedDate = parseDate(new Date(createdAt));
 
-            <innerDiv>
-              <OrderItem><b>Total Cost:</b> $ {totalCost}</OrderItem>
-              <OrderItem><b>Created at:</b> {createdAt}</OrderItem>
-            </innerDiv>
-            <br/>
-          </Division>
-          <hr/>
-        </div>
-      </Link>
-    </Container>
-  );
+	return (
+		<Container>
+			<Link
+				to="/customer/order"
+				style={{ textDecoration: "none", color: "black" }}
+			>
+				<div>
+					<OrderTitle>Order {orderId} </OrderTitle>
+					<hr />
+					<Division>
+						<innerDiv>
+							<OrderList>
+								<OrderItem>
+									<b>Vendor:</b> {vendorId.name}
+								</OrderItem>
+								<OrderItem>
+									<b>Status:</b> {status}
+								</OrderItem>
+							</OrderList>
+						</innerDiv>
+
+						<innerDiv>
+							<OrderItem>
+								<b>Items ordered: </b>
+							</OrderItem>
+							{Object.keys(foodItems).map((key, i) => (
+								<FoodItem>
+									{foodItems[key]} {key}
+								</FoodItem>
+							))}
+						</innerDiv>
+
+						<innerDiv>
+							<OrderItem>
+								<b>Total Cost:</b> $ {totalCost}
+							</OrderItem>
+							<OrderItem>
+								<b>Created at:</b> {parsedDate}
+							</OrderItem>
+						</innerDiv>
+						<br />
+					</Division>
+					<hr />
+				</div>
+			</Link>
+		</Container>
+	);
 }
