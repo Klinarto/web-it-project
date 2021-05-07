ee59445656c29b9ea685e190d765ba2fcb4f24c1
klinarto <kevinlinarto@gmail.com>
2021-05-07 20:31:10 +0800

    Added comments

--- client/src/customer-app/components/Login.js
+++ client/src/customer-app/components/Login.js
@@ -13,119 +13,131 @@ import { Alert } from "@material-ui/lab";
 import { AuthContext } from "../../shared/auth-context";
 
 export default function SignIn() {
-  const auth = useContext(AuthContext);
-  const history = useHistory();
+	const auth = useContext(AuthContext);
+	const history = useHistory();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [open, setOpen] = useState(false);
-  const [snackbar, setSnackbar] = useState({});
-  const duration = 3000;
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-  // send username and password on form submission
-  const sendData = async () => {
-    const data = { email: email, password: password };
-    console.log(data);
-    try {
-      const res = await axios.post("/customer/login", data, {
-        headers: { "Content-Type": "application/json" },
-      });
-      setOpen(true);
-      setSnackbar({
-        data: "Login successful",
-        severity: "success",
-      });
-      console.log(res);
-      console.log(res.data);
+	// open state for materialUI snackbar
+	const [open, setOpen] = useState(false);
 
-      // store token
-      // localStorage.setItem("token", res.data.token);
-      auth.login(res.data.token);
-      history.push("/customer/menu");
-    } catch (error) {
-      console.log(error);
-      setOpen(true);
-      setSnackbar({
-        data: error.response.data,
-        severity: "error",
-      });
-    }
-  };
+	// details for materialUI snackbar
+	const [snackbar, setSnackbar] = useState({});
 
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setOpen(false);
-  };
+	// duration for snackbar
+	const duration = 3000;
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <Snackbar
-        anchorOrigin={{ vertical: "top", horizontal: "center" }}
-        open={open}
-        autoHideDuration={duration}
-        onClose={handleClose}
-      >
-        <Alert severity={snackbar.severity} onClose={handleClose}>
-          {snackbar.data}
-        </Alert>
-      </Snackbar>
-      <div>
-        <form noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            autoComplete="email"
-            autoFocus
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+	// send username and password on form submission
+	const sendData = async () => {
+		// create a data object for axios post
+		const data = { email: email, password: password };
+		console.log(data);
+		try {
+			const res = await axios.post("/customer/login", data, {
+				headers: { "Content-Type": "application/json" },
+			});
+			// set snackbar details
+			setOpen(true);
+			setSnackbar({
+				data: "Login successful",
+				severity: "success",
+			});
 
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            // type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            style={{
-              backgroundColor: "#000",
-              padding: "16px 0",
-            }}
-            onClick={() => {
-              sendData();
-            }}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item>
-              <Link to="/customer/register">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
-  );
+			console.log(res);
+			console.log(res.data);
+
+			// store token
+			// localStorage.setItem("token", res.data.token);
+			auth.login(res.data.token);
+			history.push("/customer/menu");
+		} catch (error) {
+			console.log(error);
+
+			// set snackbar details
+			setOpen(true);
+			setSnackbar({
+				data: error.response.data,
+				severity: "error",
+			});
+		}
+	};
+
+	// handleclose template from materialUI docs
+	const handleClose = (event, reason) => {
+		if (reason === "clickaway") {
+			return;
+		}
+		setOpen(false);
+	};
+
+	return (
+		<Container component="main" maxWidth="xs">
+			<Snackbar
+				anchorOrigin={{ vertical: "top", horizontal: "center" }}
+				open={open}
+				autoHideDuration={duration}
+				onClose={handleClose}
+			>
+				<Alert severity={snackbar.severity} onClose={handleClose}>
+					{snackbar.data}
+				</Alert>
+			</Snackbar>
+			<div>
+				<form noValidate>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						id="email"
+						label="Email Address"
+						autoComplete="email"
+						autoFocus
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						label="Password"
+						type="password"
+						id="password"
+						autoComplete="current-password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+
+					<FormControlLabel
+						control={<Checkbox value="remember" color="primary" />}
+						label="Remember me"
+					/>
+					<Button
+						// type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
+						style={{
+							backgroundColor: "#000",
+							padding: "16px 0",
+						}}
+						onClick={() => {
+							sendData();
+						}}
+					>
+						Sign In
+					</Button>
+					<Grid container>
+						<Grid item>
+							<Link to="/customer/register">
+								{"Don't have an account? Sign Up"}
+							</Link>
+						</Grid>
+					</Grid>
+				</form>
+			</div>
+		</Container>
+	);
 }
--- client/src/customer-app/pages/Vans.js
+++ client/src/customer-app/pages/Vans.js
@@ -2,6 +2,8 @@ import React, { Fragment } from "react";
 import Map from "../components/Map";
 
 export default function Vans() {
+	// display vendor buttons where when its clicked
+
 	return (
 		<Fragment>
 			<Map />
