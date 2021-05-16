4aff1ebb69496dd2231686cb75a5b1d2bc103133
Sonia Tam <soniat@student.unimelb.edu.au>
2021-05-16 17:30:27 +1000

    welcome page (copied from customer)

---
+++ client/src/vendor-app/components/Login.js
@@ -0,0 +1,143 @@
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import axios from "axios";
+import { Snackbar } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+import { AuthContext } from "../../shared/auth-context";
+
+export default function SignIn() {
+	const auth = useContext(AuthContext);
+	const history = useHistory();
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	// open state for materialUI snackbar
+	const [open, setOpen] = useState(false);
+
+	// details for materialUI snackbar
+	const [snackbar, setSnackbar] = useState({});
+
+	// duration for snackbar
+	const duration = 3000;
+
+	// send username and password on form submission
+	const sendData = async () => {
+		// create a data object for axios post
+		const data = { email: email, password: password };
+		console.log(data);
+		try {
+			const res = await axios.post("/vendor/login", data, {
+				headers: { "Content-Type": "application/json" },
+			});
+			// set snackbar details
+			setOpen(true);
+			setSnackbar({
+				data: "Login successful",
+				severity: "success",
+			});
+
+			console.log(res);
+			console.log(res.data);
+
+			// store token
+			// localStorage.setItem("token", res.data.token);
+			auth.login(res.data.token);
+			history.push("/vendor/orderlist");
+		} catch (error) {
+			console.log(error);
+
+			// set snackbar details
+			setOpen(true);
+			setSnackbar({
+				data: error.response.data,
+				severity: "error",
+			});
+		}
+	};
+
+	// handleclose template from materialUI docs
+	const handleClose = (event, reason) => {
+		if (reason === "clickaway") {
+			return;
+		}
+		setOpen(false);
+	};
+
+	return (
+		<Container component="main" maxWidth="xs">
+			<Snackbar
+				anchorOrigin={{ vertical: "top", horizontal: "center" }}
+				open={open}
+				autoHideDuration={duration}
+				onClose={handleClose}
+			>
+				<Alert severity={snackbar.severity} onClose={handleClose}>
+					{snackbar.data}
+				</Alert>
+			</Snackbar>
+			<div>
+				<form noValidate>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						id="email"
+						label="Van name"
+						autoComplete="email"
+						autoFocus
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+					<TextField
+						variant="outlined"
+						margin="normal"
+						required
+						fullWidth
+						label="Password"
+						type="password"
+						id="password"
+						autoComplete="current-password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+
+					<FormControlLabel
+						control={<Checkbox value="remember" color="primary" />}
+						label="Remember me"
+					/>
+					<Button
+						// type="submit"
+						fullWidth
+						variant="contained"
+						color="primary"
+						style={{
+							backgroundColor: "#000",
+							padding: "16px 0",
+						}}
+						onClick={() => {
+							sendData();
+						}}
+					>
+						Sign In
+					</Button>
+					<Grid container>
+						<Grid item>
+							<Link to="/vendor/register">
+								{"Don't have an account? Sign Up"}
+							</Link>
+						</Grid>
+					</Grid>
+				</form>
+			</div>
+		</Container>
+	);
+}
---
+++ client/src/vendor-app/components/Register.js
@@ -0,0 +1,157 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
+import { Snackbar } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+
+export default function Register() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // open state for materialUI snackbar
+  const [open, setOpen] = useState(false);
+
+  // details for materialUI snackbar
+  const [snackbar, setSnackbar] = useState({});
+
+  // duration for snackbar
+  const duration = 3000;
+
+  const sendData = async () => {
+    // create a data object to register new customer
+    const data = {
+      firstName,
+      lastName,
+      email,
+      password,
+    };
+
+    console.log(data);
+
+    const res = await axios.post("/customer/register", data, {
+      headers: { "Content-Type": "application/json" },
+    });
+    try {
+      // set snackbar details
+      setOpen(true);
+      setSnackbar({
+        data: "Successfully registered",
+        severity: "success",
+      });
+      console.log(res);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+
+      // set snackbar details
+      setOpen(true);
+      setSnackbar({
+        data: error.response.data,
+        severity: "error",
+      });
+    }
+  };
+
+  // handleclose template from materialUI docs
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <div>
+        <Snackbar
+          anchorOrigin={{ vertical: "top", horizontal: "center" }}
+          open={open}
+          autoHideDuration={duration}
+          onClose={handleClose}
+        >
+          <Alert severity={snackbar.severity} onClose={handleClose}>
+            {snackbar.data}
+          </Alert>
+        </Snackbar>
+        <form noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Grid>
+          </Grid>
+
+          <Button
+            // type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            style={{
+              backgroundColor: "#000",
+              padding: "16px 0",
+              marginTop: "16px",
+            }}
+            onClick={sendData}
+          >
+            Register
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+}
