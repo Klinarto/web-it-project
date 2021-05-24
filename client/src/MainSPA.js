62dccb538b7ec6bbb522b1c858ae2fef0ae5f3c0
soosoo3 <sooyoun1999@gmail.com>
2021-05-24 22:34:44 +1000

    mainSPA structure done

--- client/src/MainSPA.js
+++ client/src/MainSPA.js
@@ -1,44 +1,85 @@
+
+
 import React from "react";
 import styled from "styled-components";
+import Link from "react-router-dom/Link";
 
 const Wrapper = styled.div`
-  background-color: white;
   box-sizing: border-box;
-  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+  width: 700px;
+  overflow: hidden;
+  display: flex;
+
 `;
-const Title = styled.div`
+
+const Title = styled.h1`
   min-height: 7vh;
 `;
 
-const HeroImage = styled.div`
-  background-image: url("./coffeeBackgroundMain.jpg");
-  width: 100%;
-  height: 100%;
-  display: inline-block;
-  vertical-align: middle;
-  line-height: normal;
+const LeftContainer = styled.div`
+  width: 50%;
+  padding: 30px 50px;
+
+`;
+const RightContainer = styled.div`
+  width: 50%;
+  padding: 30px 50px;
+
+`;
+const MyButton = styled.button`
+
 `;
 
-const Flexbox = styled.div`
-  color: black;
-  text-align: center;
-  line-height: 3vh;
-`;
+// const HeroImage = styled.div`
+//   background-image: url("./coffeeBackgroundMain.jpg");
+//   width: 100%;
+//   height: 100%;
+//   display: inline-block;
+//   vertical-align: middle;
+//   line-height: normal;
+// `;
 
-const Detail = styled.div`
-  color: black;
-  text-align: center;
-  line-height: 3vh;
-`;
+// const Flexbox = styled.div`
+//   color: black;
+//   text-align: center;
+//   line-height: 3vh;
+// `;
+
+// const Detail = styled.div`
+//   color: black;
+//   text-align: center;
+//   line-height: 3vh;
+// `;
 
 export function MainSPA() {
   return (
     <Wrapper>
-      <Title>Login</Title>
-      <HeroImage>
-        <Flexbox>twologins</Flexbox>
-      </HeroImage>
-      <Detail>asdf</Detail>
+      <LeftContainer>
+        <Title>
+          Vendor login
+        </Title>
+        <Link to={"/vendor/login"}>
+          <MyButton>
+            button
+          </MyButton>
+        </Link>
+      </LeftContainer>
+      <RightContainer>
+        <Title>
+          Customer login
+        </Title>
+        <Link to={"/customer/login"}>
+          <MyButton>
+            button
+        </MyButton>
+        </Link>
+
+      </RightContainer>
     </Wrapper>
   );
 }
