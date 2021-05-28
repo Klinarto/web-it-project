8b2ff27cf470d536b68000750432e04dfde799d6
klinarto <kevinlinarto@gmail.com>
2021-05-28 19:45:14 +0800

    implemented snackbar for changing password and profile

--- client/src/customer-app/components/ChangePassword.js
+++ client/src/customer-app/components/ChangePassword.js
@@ -2,132 +2,179 @@ import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
-import {Edit, ButDiv, BackDiv,Line, Division} from "../pages/MyAccount.style";
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import {
+	Edit,
+	ButDiv,
+	BackDiv,
+	Line,
+	Division,
+} from "../pages/MyAccount.style";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import axios from "axios";
+import { Snackbar } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 
+export default function ChangePassword({ changePassword }) {
+	const [currPass, setCurrPass] = useState("");
+	const [newPass, setNewPass] = useState("");
+	const [confirmPass, setConfirmPass] = useState("");
+	const [same, setSame] = useState(false);
 
-export default function ChangePassword({changePassword}) {
-    const [currPass, setCurrPass] = useState("");
-    const [newPass, setNewPass] = useState("");
-    const [confirmPass, setConfirmPass] = useState("");
-    const [same , setSame]=useState(false);
+	// open state for materialUI snackbar
+	const [open, setOpen] = useState(false);
 
+	// details for materialUI snackbar
+	const [snackbar, setSnackbar] = useState({});
 
-    const updatePassword = async (updated) => {
+	// duration for snackbar
+	const duration = 3000;
+
+	const updatePassword = async (updated) => {
 		try {
 			console.log(JSON.parse(updated));
 			const res = await axios.put("/customer/me/password", JSON.parse(updated));
 			console.log(res);
+			// set snackbar details
+			setOpen(true);
+			setSnackbar({
+				data: "Password changed",
+				severity: "success",
+			});
 		} catch (error) {
-
-			console.log(error.response.data);
+			console.log(error);
+			// set snackbar details
+			setOpen(true);
+			setSnackbar({
+				data: error.response.data,
+				severity: "error",
+			});
 		}
 		return;
 	};
 
-    
-    const [passwordHelper, setPasswordHelper] = useState("");
-  
-    const onChange = (e) => {
-      let validAlpha;
-      let validDigit;
-      let validLength;
-  
-    switch (e.target.id) {
-        case "New Password":
-          setNewPass(e.target.value);
-          validAlpha = /^.*[a-zA-Z][^a-zA-Z]*$/.test(e.target.value);
-          validDigit = /^.*[0-9][^0-9]*$/.test(e.target.value);
-          validLength = /^.{8,}$/.test(e.target.value);
-    
-          if (!validAlpha) {
-            setPasswordHelper("Password should have at least one alphabet (a-Z)");
-          } else if (!validDigit) {
-            setPasswordHelper("Password should have at least one number (0)");
-          } else if (!validLength) {
-            setPasswordHelper("Passwrod should be at least 8 characters long");
-          } else {
-            setPasswordHelper("");
-          }
-          break
-      }
-    };
+	// handleclose template from materialUI docs
+	const handleClose = (event, reason) => {
+		if (reason === "clickaway") {
+			return;
+		}
+		setOpen(false);
+	};
 
-    function differentPassword(){
-        setSame(true);
-    }
+	const [passwordHelper, setPasswordHelper] = useState("");
 
-    return(
-        <Division>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                <TextField
-                    name="Current Password"
-                    variant="outlined"
-                    fullWidth
-                    id="Current Password"
-                    type="password"
-                    label="Current Password"
-                    autoFocus
-                    value={currPass}
-                    onChange={(e) => setCurrPass(e.target.value)}
-                />
-                </Grid>
-                <Grid item xs={12}>
-                <TextField
-                    name="New Password"
-                    variant="outlined"
-                    fullWidth
-                    id="New Password"
-                    type="password"
-                    label="New Password"
-                    autoFocus
-                    value={newPass}
-                    onChange={onChange}
-                    error={passwordHelper.length !== 0}
-                    helperText={passwordHelper}
-                />
-                </Grid>
-                <Grid item xs={12}>
-                <TextField
-                    variant="outlined"
-                    fullWidth
-                    type="password"
-                    id="Confirm Password"
-                    label="Confirm Password"
-                    name="Confirm Password"
-                    autoComplete="Confirm Password"
-                    value={confirmPass}
-                    onChange={(e) => setConfirmPass(e.target.value)}
+	const onChange = (e) => {
+		let validAlpha;
+		let validDigit;
+		let validLength;
 
-                />
-                </Grid>
-            </Grid>
+		switch (e.target.id) {
+			case "New Password":
+				setNewPass(e.target.value);
+				validAlpha = /^.*[a-zA-Z][^a-zA-Z]*$/.test(e.target.value);
+				validDigit = /^.*[0-9][^0-9]*$/.test(e.target.value);
+				validLength = /^.{8,}$/.test(e.target.value);
 
-            {same ? <Line>Password is Same</Line> : <Line>Passwords are different</Line>}
-            <ButDiv>
-            <BackDiv>
-            <ArrowBackIosIcon onClick={() => changePassword()}></ArrowBackIosIcon>
-            </BackDiv>
-            <Edit>
-            
-            
-            <Button 
-            variant="contained"
-            onClick={() => {
+				if (!validAlpha) {
+					setPasswordHelper("Password should have at least one alphabet (a-Z)");
+				} else if (!validDigit) {
+					setPasswordHelper("Password should have at least one number (0)");
+				} else if (!validLength) {
+					setPasswordHelper("Passwrod should be at least 8 characters long");
+				} else {
+					setPasswordHelper("");
+				}
+				break;
+		}
+	};
 
-                if(confirmPass===newPass ){
-                    differentPassword();
-                    updatePassword(`{"newPassword":"${newPass}","password":"${currPass}"}`);
-                }else{
-                    console.log(confirmPass);
-                    console.log(newPass);                }
-            }}
-            >Submit</Button>
-            
-            </Edit>
-            </ButDiv>
-        </Division>    
-    );
+	function differentPassword() {
+		setSame(true);
+	}
+
+	return (
+		<Division>
+			<Snackbar
+				anchorOrigin={{ vertical: "top", horizontal: "center" }}
+				open={open}
+				autoHideDuration={duration}
+				onClose={handleClose}
+			>
+				<Alert severity={snackbar.severity} onClose={handleClose}>
+					{snackbar.data}
+				</Alert>
+			</Snackbar>
+			<Grid container spacing={2}>
+				<Grid item xs={12}>
+					<TextField
+						name="Current Password"
+						variant="outlined"
+						fullWidth
+						id="Current Password"
+						type="password"
+						label="Current Password"
+						autoFocus
+						value={currPass}
+						onChange={(e) => setCurrPass(e.target.value)}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						name="New Password"
+						variant="outlined"
+						fullWidth
+						id="New Password"
+						type="password"
+						label="New Password"
+						autoFocus
+						value={newPass}
+						onChange={onChange}
+						error={passwordHelper.length !== 0}
+						helperText={passwordHelper}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						variant="outlined"
+						fullWidth
+						type="password"
+						id="Confirm Password"
+						label="Confirm Password"
+						name="Confirm Password"
+						autoComplete="Confirm Password"
+						value={confirmPass}
+						onChange={(e) => setConfirmPass(e.target.value)}
+					/>
+				</Grid>
+			</Grid>
+
+			{same ? (
+				<Line>Password is Same</Line>
+			) : (
+				<Line>Passwords are different</Line>
+			)}
+			<ButDiv>
+				<BackDiv>
+					<ArrowBackIosIcon onClick={() => changePassword()}></ArrowBackIosIcon>
+				</BackDiv>
+				<Edit>
+					<Button
+						variant="contained"
+						onClick={() => {
+							if (confirmPass === newPass) {
+								differentPassword();
+								updatePassword(
+									`{"newPassword":"${newPass}","password":"${currPass}"}`
+								);
+							} else {
+								console.log(confirmPass);
+								console.log(newPass);
+							}
+						}}
+					>
+						Submit
+					</Button>
+				</Edit>
+			</ButDiv>
+		</Division>
+	);
 }
--- client/src/customer-app/components/EditProfile.js
+++ client/src/customer-app/components/EditProfile.js
@@ -2,111 +2,152 @@ import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
-import {Edit, ButDiv, BackDiv, Division} from "../pages/MyAccount.style";
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { Edit, ButDiv, BackDiv, Division } from "../pages/MyAccount.style";
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import axios from "axios";
-
+import { Snackbar } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 
 export default function EditProfile(props) {
-    const changeState =props.changeState
-    const { email, lastName, firstName } = props.customer;
-    
-    const [upFirstName, setFirstName] = useState(firstName);
-    const [upLastName, setLastName] = useState(lastName);
-    const [upEmail, setEmail] = useState(email);
+	const changeState = props.changeState;
+	const { email, lastName, firstName } = props.customer;
 
-    const [emailHelper, setEmailHelper] = useState("");
+	const [upFirstName, setFirstName] = useState(firstName);
+	const [upLastName, setLastName] = useState(lastName);
+	const [upEmail, setEmail] = useState(email);
 
-    const onChange = (e) => {
-        let valid;
-    
-    switch (e.target.id) {
-        case "email":
-          setEmail(e.target.value);
-          valid = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
-            e.target.value
-          );
-  
-          if (!valid) {
-            setEmailHelper("Invalid Email");
-          } else {
-            setEmailHelper("");
-          }
-          break;
-        }
-    };
+	const [emailHelper, setEmailHelper] = useState("");
 
-    const updateProfile = async (updated) => {
+	// open state for materialUI snackbar
+	const [open, setOpen] = useState(false);
+
+	// details for materialUI snackbar
+	const [snackbar, setSnackbar] = useState({});
+
+	// duration for snackbar
+	const duration = 3000;
+
+	const onChange = (e) => {
+		let valid;
+
+		switch (e.target.id) {
+			case "email":
+				setEmail(e.target.value);
+				valid = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+					e.target.value
+				);
+
+				if (!valid) {
+					setEmailHelper("Invalid Email");
+				} else {
+					setEmailHelper("");
+				}
+				break;
+		}
+	};
+
+	const updateProfile = async (updated) => {
 		try {
 			console.log(JSON.parse(updated));
 			const res = await axios.put("/customer/me", JSON.parse(updated));
 			console.log(res);
+			// set snackbar details
+			setOpen(true);
+			setSnackbar({
+				data: "Profile updated",
+				severity: "success",
+			});
 		} catch (error) {
-			console.log(error.response.data);
+			console.log(error);
+			// set snackbar details
+			setOpen(true);
+			setSnackbar({
+				data: error.response.data,
+				severity: "error",
+			});
 		}
 		return;
 	};
 
-    return(
-        <Division>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                <TextField
-                    autoComplete="fname"
-                    name="firstName"
-                    variant="outlined"
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    autoFocus
-                    value={upFirstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                />
-                </Grid>
-                <Grid item xs={12}>
-                <TextField
-                    variant="outlined"
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="lname"
-                    value={upLastName}
-                    onChange={(e) => setLastName(e.target.value)}
-             
-                />
-                </Grid>
-                <Grid item xs={12}>
-                <TextField
-                    variant="outlined"
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                    value={upEmail}
-                    onChange={onChange}
-                    error={emailHelper.length !== 0}
-                    helperText={emailHelper}
-                />
-                </Grid>
-            </Grid>
-            <ButDiv>
-            <BackDiv>
-            <ArrowBackIosIcon onClick={() => changeState()}></ArrowBackIosIcon>
-            </BackDiv>
-            <Edit>
-            <Button 
-            color="primary"
-            
-            onClick={() => {
-                updateProfile(`{"firstName":"${upFirstName}","lastName":"${upLastName}", "email":"${upEmail}"}`);
-                window.location.reload()}}
-            >Submit</Button>
-            </Edit>
+	// handleclose template from materialUI docs
+	const handleClose = (event, reason) => {
+		if (reason === "clickaway") {
+			return;
+		}
+		setOpen(false);
+	};
 
-            </ButDiv>
-        </Division>
-        );
+	return (
+		<Division>
+			<Snackbar
+				anchorOrigin={{ vertical: "top", horizontal: "center" }}
+				open={open}
+				autoHideDuration={duration}
+				onClose={handleClose}
+			>
+				<Alert severity={snackbar.severity} onClose={handleClose}>
+					{snackbar.data}
+				</Alert>
+			</Snackbar>
+			<Grid container spacing={2}>
+				<Grid item xs={12}>
+					<TextField
+						autoComplete="fname"
+						name="firstName"
+						variant="outlined"
+						fullWidth
+						id="firstName"
+						label="First Name"
+						autoFocus
+						value={upFirstName}
+						onChange={(e) => setFirstName(e.target.value)}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						variant="outlined"
+						fullWidth
+						id="lastName"
+						label="Last Name"
+						name="lastName"
+						autoComplete="lname"
+						value={upLastName}
+						onChange={(e) => setLastName(e.target.value)}
+					/>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						variant="outlined"
+						fullWidth
+						id="email"
+						label="Email Address"
+						name="email"
+						autoComplete="email"
+						value={upEmail}
+						onChange={onChange}
+						error={emailHelper.length !== 0}
+						helperText={emailHelper}
+					/>
+				</Grid>
+			</Grid>
+			<ButDiv>
+				<BackDiv>
+					<ArrowBackIosIcon onClick={() => changeState()}></ArrowBackIosIcon>
+				</BackDiv>
+				<Edit>
+					<Button
+						color="primary"
+						onClick={() => {
+							updateProfile(
+								`{"firstName":"${upFirstName}","lastName":"${upLastName}", "email":"${upEmail}"}`
+							);
+							window.location.reload();
+						}}
+					>
+						Submit
+					</Button>
+				</Edit>
+			</ButDiv>
+		</Division>
+	);
 }
-
