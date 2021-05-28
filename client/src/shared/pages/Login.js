99fcc27d7fe9916dd2aa35fdc143e91deaea8249
John Kim <kakwak123@gmail.com>
2021-05-29 02:49:36 +1000

    fixed error and reverted jest

--- client/src/shared/pages/Login.js
+++ client/src/shared/pages/Login.js
@@ -2,11 +2,11 @@ import React from "react";
 import { useHistory, useLocation } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import {
-	Container,
-	LeftWrapper,
-	RightImage,
-	LeftImage,
-	Title,
+  Container,
+  LeftWrapper,
+  RightImage,
+  LeftImage,
+  Title,
 } from "./Welcome.style";
 import coffeeBackground from "../../images/coffeeBackground.png";
 // import SignIn from "../components/Login";
@@ -18,56 +18,56 @@ import { Fragment } from "react";
 // Login page, left has the component of login page, which is being imported from the components folder,
 // Right side is a image in a flex-box to have 4:6 ratio
 export function Login() {
-	const history = useHistory();
-	const goBack = () => {
-		if (pathname.includes("vendor")) {
-			history.push("/customer/login");
-		} else {
-			history.push("/vendor/login");
-		}
-	};
+  const history = useHistory();
+  const goBack = () => {
+    if (pathname.includes("vendor")) {
+      history.push("/customer/login");
+    } else {
+      history.push("/vendor/login");
+    }
+  };
 
-	const pathname = useLocation().pathname;
+  const pathname = useLocation().pathname;
 
-	let user = "customer";
+  let user = "customer";
 
-	if (pathname.includes("vendor")) {
-		user = "vendor";
-	}
+  if (pathname.includes("vendor")) {
+    user = "vendor";
+  }
 
-	const renderLoginText = () => {
-		let headingMsg = "Customer Login";
-		let changeUser = "Are you a Vendor?";
-		let changeUserButtonMsg = "Go to Vendor Login";
-		if (user == "vendor") {
-			headingMsg = "Vendor Login";
-			changeUser = "Are you a Customer?";
-			changeUserButtonMsg = "Go to Customer Login";
-		}
-		return (
-			<Fragment>
-				<h1>{headingMsg}</h1>
-				<h4>{changeUser}</h4>
-				<Button type="button" onClick={goBack}>
-					{changeUserButtonMsg}
-				</Button>
-			</Fragment>
-		);
-	};
+  const renderLoginText = () => {
+    let headingMsg = "Customer Login";
+    let changeUser = "Are you a Vendor?";
+    let changeUserButtonMsg = "Go to Vendor Login";
+    if (user == "vendor") {
+      headingMsg = "Vendor Login";
+      changeUser = "Are you a Customer?";
+      changeUserButtonMsg = "Go to Customer Login";
+    }
+    return (
+      <Fragment>
+        <h1>{headingMsg}</h1>
+        <h4>{changeUser}</h4>
+        <Button type="button" onClick={goBack}>
+          {changeUserButtonMsg}
+        </Button>
+      </Fragment>
+    );
+  };
 
-	return (
-		<Container>
-			<LeftWrapper>
-				{renderLoginText()}
+  return (
+    <Container>
+      <LeftWrapper>
+        {renderLoginText()}
 
-				<br />
-				<LeftImage alt="cookie-logo" src={cookieLogo} />
-				<Title>Snacks in a Van</Title>
-				<SignIn />
-			</LeftWrapper>
-			<RightImage alt="coffee-image" src={coffeeBackground} />
-		</Container>
-	);
+        <br />
+        <LeftImage alt="cookie-logo" src={cookieLogo} />
+        <Title>Snacks in a Van</Title>
+        <SignIn />
+      </LeftWrapper>
+      <RightImage alt="coffee-image" src={coffeeBackground} />
+    </Container>
+  );
 }
 
 export default Login;
