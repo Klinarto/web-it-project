9ce64020d6e8bf881a7f7ba6880fedd52582d001
soosoo3 <sooyoun1999@gmail.com>
2021-05-30 00:18:55 +1000

    ss

--- client/src/customer-app/pages/Vans.js
+++ client/src/customer-app/pages/Vans.js
@@ -1,48 +1,116 @@
-import React, { useState, Fragment, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Map from "../../shared/components/Map";
 import axios from "axios";
-import { Wrapper } from "../pages/Vans.style";
+// import { Wrapper, PopUpHeader, PopUpBody, PopUpTitle, PopUpCloseButton } from "../pages/Vans.style";
+import { Fragment } from "react";
+import { Dialog } from "@material-ui/core";
 
 export default function SimpleModal() {
-  const [vendors, setVendors] = useState([]);
-  const [selected, setSelected] = useState(null);
+	const [vendors, setVendors] = useState([]);
+	const [modalStyle] = React.useState(getModalStyle);
 
-  useEffect(() => {
-    // used for cleanup
-    let isMounted = true;
+	const [selected, setSelected] = useState(null);
+	const [open, setOpen] = React.useState(false);
 
-    // fetch list of vendors from db
-    const fetchVendors = async () => {
-      try {
-        const res = await axios.get("/vendor");
-        if (isMounted) {
-          setVendors(res.data);
-          // console.log(vendors);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
+	const handleOpen = () => {
+		setOpen(true);
+	};
 
-    fetchVendors();
+	const handleClose = () => {
+		setOpen(false);
+	};
+	function getModalStyle() {
+		const top = 50;
+		const left = 50;
 
-    return () => {
-      isMounted = false;
-    };
-  }, [vendors]);
+		return {
+			top: `${top}%`,
+			left: `${left}%`,
+			transform: `translate(-${top}%, -${left}%)`,
+		};
+	}
+	const body = (
+		<div style={modalStyle}>
+			<h2 id="simple-modal-title">Text in a modal</h2>
+			<p id="simple-modal-description">
+				Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+		  </p>
+			<SimpleModal />
+		</div>
+	);
+	useEffect(() => {
+		// used for cleanup
+		let isMounted = true;
 
-  return (
-    <Fragment>
-      <Map data={vendors} selected={selected} setSelected={setSelected} />
+		// fetch list of vendors from db
+		const fetchVendors = async () => {
+			try {
+				const res = await axios.get("/vendor");
+				if (isMounted) {
+					setVendors(res.data);
+					// console.log(vendors);
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		};
 
-      {selected ? (
-        <Wrapper>
-          <div style={{}}>
-            <h1>{selected.name}</h1>
-            <button onClick={() => setSelected(null)}>&times;</button>
-          </div>
-        </Wrapper>
-      ) : null}
-    </Fragment>
-  );
+		fetchVendors();
+
+		return () => {
+			isMounted = false;
+		};
+	}, [vendors]);
+
+	return (
+		<Fragment >
+			<Map data={vendors} selected={selected} setSelected={setSelected} onClick={() => setSelected(null)} />
+			<Dialog
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="simple-modal-title"
+				aria-describedby="simple-modal-description"
+			>
+				{body}
+			</Dialog>
+			{selected ?
+
+				handleOpen()
+
+				// <Wrapper >
+
+				// 	<PopUpHeader >
+				// 		<PopUpTitle>{selected.name}</PopUpTitle>
+				// 		<PopUpCloseButton onClick={() => setSelected(null)}>&times;</PopUpCloseButton>
+				// 	</PopUpHeader>
+
+				// 	<PopUpBody  >
+				// 		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				// 	</PopUpBody>
+				// </Wrapper>
+				: handleClose()}
+		</Fragment>
+	);
 }
+
+// function Modal({ children, shown, close }) {
+// 	return shown ? (
+// 		<Aaaa
+// 			onClick={() => {
+// 				// close modal when outside of modal is clicked
+// 				close();
+// 			}}
+// 		>
+// 			<Bbbb
+// 				className="modal-content"
+// 				onClick={e => {
+// 					// do not close modal if anything inside modal content is clicked
+// 					e.stopPropagation();
+// 				}}
+// 			>
+// 				<button onClick={close}>Close</button>
+// 				{children}
+// 			</Bbbb>
+// 		</Aaaa>
+// 	) : null;
+// }
--- client/src/customer-app/pages/Vans.style.js
+++ client/src/customer-app/pages/Vans.style.js
@@ -1,26 +1,84 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  background-color: blue;
   position: absolute;
-  transform: translate(-50%, 50%);
+  transform: translate(-50%, 50%) ;
   left: 50%;
   bottom: 50%;
+  transition: 3s ease-in-out;
+  z-index: 10;
+  background-color: white;
+  width: 500px;
+  max-width: 80%;
+  border: 1px solid black;
+  border-radius: 10px;
+  &:active{
+    transform: translate(-50%, -50%) scale(1);
+  }
 `;
-export const VanName = styled.div``;
+export const PopUpHeader = styled.div`
+ padding: 10px 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid black;
+`;
 
-// <div style={{
-// 				backgroundColor: "blue", position: "absolute",
-// 				left: "50%", transform: `translate(-50%, -50%)`,
-// 				top: "50%"
-// 			}}>
 
-// 				{selected ?
-// 					<div style={{}}>
-// 						<h1>{selected.name}</h1>
-// 						<button onClick={() => setSelected(null)}>
-// 							&times;
-// 								</button>
-// 					</div>
-// 					: null}
-// 			</div>
+export const PopUpTitle = styled.div`
+ font-size: 1.25rem;
+  font-weight: bold;
+`;
+
+export const PopUpCloseButton = styled.button`
+cursor: pointer;
+  border: none;
+  outline: none;
+  background: none;
+  font-size: 1.25rem;
+  font-weight: bold;
+`;
+
+
+export const PopUpBody = styled.div`
+  padding: 10px 15px;
+
+`;
+
+export const Overlay = styled.div`
+  position: fixed;
+  opacity: 0;
+  transition: 200ms ease-in-out;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, .5);
+  pointer-events: none;
+  &::active{
+    opacity: 1;
+  pointer-events: all;
+  }
+`;
+
+
+export const Aaaa = styled.div`
+ position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.4);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+`;
+export const Bbbb = styled.div`
+  width: 60%;
+  min-height: 200px;
+  background-color: white;
+  padding: 25px;
+`;
+
+
