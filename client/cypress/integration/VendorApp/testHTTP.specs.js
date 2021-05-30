b5ae6415b889aabb8b460895080d2885d29bd7f5
John Kim <kakwak123@gmail.com>
2021-05-30 23:25:43 +1000

    Update testHTTP.specs.js

--- client/cypress/integration/VendorApp/testHTTP.specs.js
+++ client/cypress/integration/VendorApp/testHTTP.specs.js
@@ -1,7 +1,26 @@
-cy.intercept(
-  {
-    method: "GET", // Route all GET requests
-    url: "/users/*", // that have a URL that matches '/users/*'
-  },
-  [] // and force the response to be: []
-);
+/// <reference types="cypress" />
+
+context("Actions", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/vendor");
+  });
+
+  // https://on.cypress.io/interacting-with-elements
+
+  it("test if put request was sent fine", () => {
+    // https://on.cypress.io/type
+
+    cy.request({
+      method: "GET",
+      url: "http://localhost:5000/vendor",
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+    }).then((resp) => {
+      // redirect status code is 200
+      expect(resp.status).to.eq(200);
+    });
+
+    //change status
+  });
+});
